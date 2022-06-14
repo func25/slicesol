1975e193fae0bd359af55556a20315bdbc31853b
phuong <phuong@wolffun>
2022-06-14 20:15:19 +0700

    add MapID func

--- slicesol/map.go
+++ slicesol/map.go
@@ -15,6 +15,21 @@ func Map[T, K any](a []T, fn func(e T) K) []K {
 	return res
 }
 
+// MapID transforms each element of input slice through fn and return a new transformed slice
+func (t Sliol[T]) MapID(fn func(e T, id int) T) Sliol[T] {
+	return MapID(t, fn)
+}
+
+// MapID transforms each element of input slice through fn and return a new transformed slice
+func MapID[T, K any](a []T, fn func(e T, id int) K) []K {
+	res := make([]K, len(a))
+	for k, v := range a {
+		res[k] = fn(v, k)
+	}
+
+	return res
+}
+
 // MapWithError transforms each element of input slice through fn and return a new transformed slice
 //It were interupted if any error occured
 func (t Sliol[T]) MapErr(fn func(e T) (T, error)) (Sliol[T], error) {
